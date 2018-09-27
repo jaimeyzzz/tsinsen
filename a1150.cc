5b8f26000933126ab3812667f2b61054b3fe8903
loyave <loyaveforever@gmail.com>
2018-09-27 12:43:41 +0800

    a1150

--- a1150.cc
+++ a1150.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -5,23 +6,43 @@ using namespace std;
 int n, m;
 int f[10010];
 
-void swap(int p, int k) {
-    int cnt = 0, step = 1;
-    for (int i = n - 2; i >= 0; i --) {
-        int cur = f[i];
-        step *= n - i - 1;
-        for (int j = i + 1; j < n; j ++) {
-            if (f[j] < f[i]) {
-                cnt += step;
-            }
-            if (cnt > m) {
+void swap(int i, int j) {
+    int tmp = f[i];
+    f[i] = f[j];
+    f[j] = tmp;
+}
 
-            }
-
-        }
-
+void search(int p, int k) {
+    if (k == 0) {
+        return;
     }
-
+    int cnt = 0, step = 1;
+    for (int i = n - 2; i >= p; i --) {
+        if (step <= m)
+            step *= n - i - 1;
+        for (int j = i + 1; j < n; j ++) {
+            if (f[i] < f[j]) {
+                if (cnt + step >= k) {
+                    swap(i, j);
+                    if (cnt + step == k) {
+                        sort(f + i + 1, f + n, [](const int& a, const int& b) {
+                            if (a > b) return true;
+                            return false;
+                        });
+                    }
+                    else {
+                        sort(f + i + 1, f + n);
+                        search(i + 1, k - cnt - 1);
+                    }
+                    return;
+                }
+                else {
+                    cnt += step;
+                }
+            }
+        }
+        sort(f + i, f + n);
+    }
 }
 
 
@@ -30,6 +51,10 @@ int main() {
     for (int i = 0; i < n; i ++) {
         cin >> f[i];
     }
-
+    search(0, m);
+    for (int i = 0; i < n; i ++) {
+        cout << f[i];
+        if (i < n - 1) cout << ' ';
+    }
     return 0;
 }
